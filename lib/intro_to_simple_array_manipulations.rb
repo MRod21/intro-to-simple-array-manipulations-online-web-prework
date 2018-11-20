1f9014a3b18ba255d7f4a7849eab9d87debe4def
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-20 04:04:50 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,13 @@ def using_push(array, string)
   array = []
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array = []
+  array.unshift(sting)
+end
+
+def using_pop(array)
+  array = []
+  array.pop 
+end
